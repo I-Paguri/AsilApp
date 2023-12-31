e8c9074a0f1143be0c7483f451d5e0d2b6501f47
Vito Marco Rubino <98357718+vitomarcorubino@users.noreply.github.com>
2023-12-31 15:39:00 +0100

    Updated floating action button in treatment fragment

--- app/src/main/java/it/uniba/dib/sms232417/asilapp/doctor/fragments/TreatmentFragment.java
+++ app/src/main/java/it/uniba/dib/sms232417/asilapp/doctor/fragments/TreatmentFragment.java
@@ -1,17 +1,9 @@
 package it.uniba.dib.sms232417.asilapp.doctor.fragments;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.content.res.Resources;
 import android.os.Bundle;
 
-import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
-
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextUtils;
-import android.text.style.BulletSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,56 +11,11 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.material.card.MaterialCardView;
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 import it.uniba.dib.sms232417.asilapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TreatmentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class TreatmentFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TreatmentFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TreatmentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TreatmentFragment newInstance(String param1, String param2) {
-        TreatmentFragment fragment = new TreatmentFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -86,20 +33,28 @@ public class TreatmentFragment extends Fragment {
         LinearLayout parentLayout = view.findViewById(R.id.linearLayoutCardView);
 
         // Convert dp to pixels
+        int dp85 = (int) (85 * Resources.getSystem().getDisplayMetrics().density);
         int dp16 = (int) (16 * Resources.getSystem().getDisplayMetrics().density);
         int dp8 = (int) (8 * Resources.getSystem().getDisplayMetrics().density);
         int dp2 = (int) (2 * Resources.getSystem().getDisplayMetrics().density);
 
-        for (int i = 0; i < 3; i++) {
+        int numberOfCards = 6;
+
+        for (int i = 0; i < numberOfCards; i++) {
             // Create a new instance of MaterialCardView
             MaterialCardView cardView = new MaterialCardView(getContext());
+
 
             // Set the layout parameters for the MaterialCardView
             LinearLayout.LayoutParams cardViewParams = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT
             );
-            cardViewParams.setMargins(dp16, dp8, dp16, dp8);
+            if (i == numberOfCards - 1) {
+                cardViewParams.setMargins(dp16, dp8, dp16, dp85);
+            } else {
+                cardViewParams.setMargins(dp16, dp8, dp16, dp8);
+            }
             cardView.setLayoutParams(cardViewParams);
 
             // Create a new LinearLayout
@@ -179,6 +134,7 @@ public class TreatmentFragment extends Fragment {
             // Add the MaterialCardView to the parent layout
             parentLayout.addView(cardView);
         }
-    }
 
+
+    }
 }
