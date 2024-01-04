eaf214664bcd8432c278db012d3f1b59f9cb0bd7
Vito Marco Rubino <98357718+vitomarcorubino@users.noreply.github.com>
2024-01-04 16:46:14 +0100

    Updated form

--- app/src/main/java/it/uniba/dib/sms232417/asilapp/doctor/fragments/TreatmentFormMedicinesFragment.java
+++ app/src/main/java/it/uniba/dib/sms232417/asilapp/doctor/fragments/TreatmentFormMedicinesFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import it.uniba.dib.sms232417.asilapp.R;
 
@@ -23,6 +25,22 @@ public class TreatmentFormMedicinesFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+
+
+        // Find the AutoCompleteTextView in the layout
+        AutoCompleteTextView medicinesList = view.findViewById(R.id.medicines_list);
+
+        // Get the string array from the resources
+        String[] medicines = getResources().getStringArray(R.array.medicines_list);
+
+        // Create an ArrayAdapter using the string array and a default layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line, medicines);
+
+        // Set the ArrayAdapter to the AutoCompleteTextView
+        medicinesList.setAdapter(adapter);
+
+
+
         super.onViewCreated(view, savedInstanceState);
     }
 }
