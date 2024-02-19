6cea3357aa20ad8cf5ef45d2442265abeb111402
Vito Marco Rubino <98357718+vitomarcorubino@users.noreply.github.com>
2024-02-19 08:45:27 +0100

    Added click listeners for the other measurements type

--- app/src/main/java/it/uniba/dib/sms232417/asilapp/doctor/fragments/MeasurementsFragment.java
+++ app/src/main/java/it/uniba/dib/sms232417/asilapp/doctor/fragments/MeasurementsFragment.java
@@ -7,7 +7,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +18,6 @@ import com.mackhartley.roundedprogressbar.ProgressTextFormatter;
 import com.mackhartley.roundedprogressbar.RoundedProgressBar;
 
 import it.uniba.dib.sms232417.asilapp.R;
-import it.uniba.dib.sms232417.asilapp.patientsFragments.AnalysesExpensesFragment;
 
 
 public class MeasurementsFragment extends Fragment {
@@ -28,8 +26,8 @@ public class MeasurementsFragment extends Fragment {
     private String patientName;
     private String patientAge;
     private String user; // Type of user: "patient" or "doctor"
-    private ArcGauge heartRateArchGauge, pressureArchGauge, lightArchGauge;
-    private MaterialCardView cardViewHeartRate, cardViewPressure, cardViewTemperature, cardViewLight;
+    private ArcGauge heartRateArchGauge, pressureArchGauge, glycemiaArchGauge;
+    private MaterialCardView cardViewHeartRate, cardViewPressure, cardViewTemperature, cardViewGlycemia;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,7 +58,7 @@ public class MeasurementsFragment extends Fragment {
         //get the gauge view
         heartRateArchGauge = view.findViewById(R.id.arcGaugeHeartRate);
         pressureArchGauge = view.findViewById(R.id.arcGaugePressure);
-        lightArchGauge = view.findViewById(R.id.arcGaugeLight);
+        glycemiaArchGauge = view.findViewById(R.id.arcGaugeLight);
 
 
         Range range = new Range();
@@ -87,9 +85,9 @@ public class MeasurementsFragment extends Fragment {
         pressureArchGauge.addRange(range2);
         pressureArchGauge.addRange(range3);
 
-        lightArchGauge.addRange(range);
-        lightArchGauge.addRange(range2);
-        lightArchGauge.addRange(range3);
+        glycemiaArchGauge.addRange(range);
+        glycemiaArchGauge.addRange(range2);
+        glycemiaArchGauge.addRange(range3);
 
         //set min max and current temperature
         heartRateArchGauge.setMinValue(40.0);
@@ -100,9 +98,9 @@ public class MeasurementsFragment extends Fragment {
         pressureArchGauge.setMaxValue(200.0);
         pressureArchGauge.setValue(50.0);
 
-        lightArchGauge.setMinValue(0.0);
-        lightArchGauge.setMaxValue(100.0);
-        lightArchGauge.setValue(150.0);
+        glycemiaArchGauge.setMinValue(0.0);
+        glycemiaArchGauge.setMaxValue(100.0);
+        glycemiaArchGauge.setValue(150.0);
 
 
         RoundedProgressBar thermometer = view.findViewById(R.id.thermometer);
@@ -150,6 +148,51 @@ public class MeasurementsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 bundle.putString("measureType", "heartRate");
+
+                MeasurementsTrendFragment measurementsTrendFragment = new MeasurementsTrendFragment();
+                measurementsTrendFragment.setArguments(bundle);
+                FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment_activity_main, measurementsTrendFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        cardViewTemperature = view.findViewById(R.id.cardViewTemperature);
+        cardViewTemperature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString("measureType", "temperature");
+
+                MeasurementsTrendFragment measurementsTrendFragment = new MeasurementsTrendFragment();
+                measurementsTrendFragment.setArguments(bundle);
+                FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment_activity_main, measurementsTrendFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        cardViewPressure = view.findViewById(R.id.cardViewBloodPressure);
+        cardViewPressure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString("measureType", "bloodPressure");
+
+                MeasurementsTrendFragment measurementsTrendFragment = new MeasurementsTrendFragment();
+                measurementsTrendFragment.setArguments(bundle);
+                FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.nav_host_fragment_activity_main, measurementsTrendFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        cardViewGlycemia = view.findViewById(R.id.cardViewGlycemia);
+        cardViewGlycemia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString("measureType", "glycemia");
 
                 MeasurementsTrendFragment measurementsTrendFragment = new MeasurementsTrendFragment();
                 measurementsTrendFragment.setArguments(bundle);
